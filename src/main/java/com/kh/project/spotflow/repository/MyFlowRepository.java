095132aff58a189be904442d59551a044ba7e45f
Junhyuk Kim <121940381+kimchoker@users.noreply.github.com>
2023-07-04 20:58:05 +0900

    다시는 날려먹지 않으리라

---
+++ src/main/java/com/kh/project/spotflow/repository/MyFlowRepository.java
@@ -0,0 +1,12 @@
+package com.kh.project.spotflow.repository;
+
+import com.kh.project.spotflow.model.entity.TimeLine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface MyFlowRepository extends JpaRepository<TimeLine, long> {
+    Optional<TimeLine> findByEmail(String email);
+}
+
