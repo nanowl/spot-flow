457792a762c999f6c083a7b81263bee7ae4ee0b1
Junhyuk Kim <121940381+kimchoker@users.noreply.github.com>
2023-07-04 21:00:19 +0900

    다시는 날려먹지 않으리라

--- src/main/java/com/kh/project/spotflow/repository/MyFlowRepository.java
+++ src/main/java/com/kh/project/spotflow/repository/MyFlowRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
-public interface MyFlowRepository extends JpaRepository<TimeLine, long> {
+public interface MyFlowRepository extends JpaRepository<TimeLine, Long> {
     Optional<TimeLine> findByEmail(String email);
 }
 
---
+++ src/main/java/com/kh/project/spotflow/service/MyFlowService.java
@@ -0,0 +1,20 @@
+package com.kh.project.spotflow.service;
+
+import com.kh.project.spotflow.model.entity.TimeLine;
+import com.kh.project.spotflow.repository.MyFlowRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MyFlowService {
+    private final MyFlowRepository repository;
+
+    public Optional<TimeLine> findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+}
