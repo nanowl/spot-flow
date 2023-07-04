a0d24055d7d5645d12e4dd0528cf5aaff2c2b963
Junhyuk Kim <121940381+kimchoker@users.noreply.github.com>
2023-07-04 21:14:52 +0900

    다시는 날려먹지 않으리라

--- src/main/java/com/kh/project/spotflow/controller/AuthController.java
+++ src/main/java/com/kh/project/spotflow/controller/AuthController.java
@@ -3,19 +3,21 @@ package com.kh.project.spotflow.controller;
 import com.kh.project.spotflow.model.dto.MemberRequestDto;
 import com.kh.project.spotflow.model.dto.MemberResponseDto;
 import com.kh.project.spotflow.model.dto.TokenDto;
+import com.kh.project.spotflow.model.entity.TimeLine;
 import com.kh.project.spotflow.service.AuthService;
+import com.kh.project.spotflow.service.MyFlowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
   private final AuthService authService;
+  private final MyFlowService myFlowService;
 
   @PostMapping("/signup")
   public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto requestDto) {
@@ -25,5 +27,10 @@ public class AuthController {
   @PostMapping("/login")
   public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto requestDto) {
     return ResponseEntity.ok(authService.login(requestDto));
+  }
+
+  @PostMapping("/myflow")
+  public ResponseEntity<Optional<TimeLine>> myFlowInfoGet(String email) {
+    return ResponseEntity.ok(myFlowService.findByEmail(email));
   }
 }
---
+++ src/main/java/com/kh/project/spotflow/model/dto/MyFlowGetDto.java
@@ -0,0 +1,10 @@
+package com.kh.project.spotflow.model.dto;
+
+public class MyFlowGetDto {
+
+    private String content;
+    private String date;
+    private String img;
+    private String location;
+
+}
