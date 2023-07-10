1cb1314fafe582b1d14b0ffbbd85dd5150070808
nanowl <hanjy20129@gmail.com>
2023-07-10 21:35:07 +0900

    diary 수정, 삭제 기능

--- src/main/java/com/kh/project/spotflow/repository/DiaryRepository.java
+++ src/main/java/com/kh/project/spotflow/repository/DiaryRepository.java
@@ -3,8 +3,6 @@ package com.kh.project.spotflow.repository;
 import com.kh.project.spotflow.model.entity.Diary;
 import com.kh.project.spotflow.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
